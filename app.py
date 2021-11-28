3bdeb8c431a5fc9e44493c83d9843c8aa8480d93
jmargutti <jacopo.margutti@gmail.com>
2021-11-28 16:22:06 +0100

    add disabilities

--- app.py
+++ app.py
@@ -4,34 +4,23 @@ from collections import OrderedDict
 import io
 import os
 from dotenv import load_dotenv
-from azure.storage.blob import BlobServiceClient
+# from azure.storage.blob import BlobServiceClient
 from flask import Flask, render_template, request, send_file
 app = Flask(__name__)
 load_dotenv()  # take environment variables from .env
-logged_in = False
 
 
-def filter_rescue_number(df_form, rescue_number=None):
+def process_data(df_form, rescue_number=None):
+
     rescues = df_form['rescue_number'].unique().tolist()
     if not rescues:
         rescues = [1]
     if not rescue_number:
         rescue_number = max(rescues)
+    rescues = [str(x) for x in rescues]
     rescues = ['total'] + rescues
     if rescue_number != 'total':
         df_form = df_form[df_form['rescue_number'] == rescue_number]
-
-    if os.path.exists('rescue-data-upload.xlsx'):
-        os.remove('rescue-data-upload.xlsx')
-    df_form.to_excel('rescue-data-upload.xlsx')
-    upload_data('rescue-data-upload.xlsx', 'website-data/rescue-data.xlsx')
-
-    return rescues, df_form
-
-
-def process_data(df_form, rescue_number=None):
-
-    rescues, df_form = filter_rescue_number(df_form, rescue_number)
 
     total = len(df_form)
     males = len(df_form[df_form['gender'] == 'male'])
@@ -43,6 +32,15 @@ def process_data(df_form, rescue_number=None):
     minors = len(df_minors)
     minors_male = len(df_minors[df_minors['gender'] == 'male'])
     minors_female = len(df_minors[df_minors['gender'] == 'female'])
+
+    # pregnant
+    pregnant, pregnant_women, pregnant_minors = 0, 0, 0
+    if 'pregnant' in df_form.columns:
+        pregnant = len(df_form[df_form['pregnant'] == 'yes'])
+    if 'pregnant' in df_adults.columns:
+        pregnant_women = len(df_adults[df_adults['pregnant'] == 'yes'])
+    if 'pregnant' in df_minors.columns:
+        pregnant_minors = len(df_minors[df_minors['pregnant'] == 'yes'])
 
     # unaccompanied minors
     df_unacc_minors = df_minors[df_minors['accompanied'] == 'no']
@@ -68,6 +66,12 @@ def process_data(df_form, rescue_number=None):
     else:
         unacc_pregnant_women = 0
 
+    # disabled
+    disabled = 0
+    if 'disabled' in df_form.columns:
+        disabled = len(df_form[df_form['disabled'] == 'yes'])
+
+    # age groups
     age_value_counts = df_form['age'].value_counts().to_dict()
     age_label_dict = {"u1": "Less than 1 year",
                       "1_4": "1-4 years",
@@ -79,6 +83,7 @@ def process_data(df_form, rescue_number=None):
         if age_label in age_value_counts.keys():
             age_group_counts[age_text] = age_value_counts[age_label]
 
+    # nationalities
     country_counts = df_form['country'].value_counts().to_dict()
     country_counts = {k: v for k, v in sorted(country_counts.items(), key=lambda item: item[1], reverse=True)}
 
@@ -89,16 +94,20 @@ def process_data(df_form, rescue_number=None):
                            minors=minors,
                            minors_male=minors_male,
                            minors_female=minors_female,
+                           pregnant=pregnant,
+                           pregnant_women=pregnant_women,
+                           pregnant_minors=pregnant_minors,
                            unacc_minors=unacc_minors,
                            unacc_minors_male=unacc_minors_male,
                            unacc_minors_female=unacc_minors_female,
                            unacc_pregnant_minors=unacc_pregnant_minors,
                            unacc_women=unacc_women,
                            unacc_pregnant_women=unacc_pregnant_women,
+                           disabled=disabled,
                            age_value_counts=age_group_counts,
                            country_counts=country_counts,
                            rescues=rescues,
-                           selected_rescue=rescue_number)
+                           selected_rescue=str(rescue_number))
 
 
 def get_data():
@@ -116,49 +125,47 @@ def get_data():
     return df_form
 
 
-@app.route("/data", methods=['GET', 'POST'])
+@app.route("/data", methods=['POST'])
 def default_page():
     if request.form['password'] == os.getenv("PASSWORD"):
-        global logged_in
-        logged_in = True
         df_form = get_data()
         return process_data(df_form)
-
-
-@app.route("/dataupdate", methods=['GET', 'POST'])
-def update_rescue():
-    global logged_in
-    if logged_in:
-        df_form = get_data()
-        return process_data(df_form, request.form['rescue'])
     else:
-        return login_page()
+        return render_template('home.html')
 
 
-def get_blob_service_client(blob_path):
-    blob_service_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION"))
-    return blob_service_client.get_blob_client(container='ocean-viking', blob=blob_path)
+@app.route("/dataupdate", methods=['POST'])
+def update_rescue():
+    if 'rescue' in request.form.keys():
+        rescue_number = request.form['rescue']
+    else:
+        rescue_number = None
+    df_form = get_data()
+    return process_data(df_form, rescue_number)
 
 
-def upload_data(data_path='rescue-data.xlsx', blob_path='website-data/rescue-data.xlsx'):
-    blob_client = get_blob_service_client(blob_path)
-    with open(data_path, "rb") as data:
-        blob_client.upload_blob(data, overwrite=True)
+# def get_blob_service_client(blob_path):
+#     blob_service_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION"))
+#     return blob_service_client.get_blob_client(container='ocean-viking', blob=blob_path)
 
 
-@app.route("/download6", methods=['GET', 'POST'])
-def download_data(data_path='rescue-data-download.xlsx', blob_path='website-data/rescue-data.xlsx'):
-    if os.path.exists(data_path):
-        os.remove(data_path)
-    blob_client = get_blob_service_client(blob_path)
-    with open(data_path, "wb") as download_file:
-        download_file.write(blob_client.download_blob().readall())
-    return send_file('rescue-data-download.xlsx', as_attachment=True, attachment_filename="rescue-data.xlsx")
+# def upload_data(data_path='rescue-data.xlsx', blob_path='website-data/rescue-data.xlsx'):
+#     blob_client = get_blob_service_client(blob_path)
+#     with open(data_path, "rb") as data:
+#         blob_client.upload_blob(data, overwrite=True)
+
+
+# @app.route("/download6", methods=['GET', 'POST'])
+# def download_data(data_path='rescue-data-download.xlsx', blob_path='website-data/rescue-data.xlsx'):
+#     if os.path.exists(data_path):
+#         os.remove(data_path)
+#     blob_client = get_blob_service_client(blob_path)
+#     with open(data_path, "wb") as download_file:
+#         download_file.write(blob_client.download_blob().readall())
+#     return send_file('rescue-data-download.xlsx', as_attachment=True, attachment_filename="rescue-data.xlsx")
 
 
 @app.route("/")
 def login_page():
-    global logged_in
-    logged_in = False
     return render_template('home.html')
 
