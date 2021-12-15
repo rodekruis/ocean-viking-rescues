6af18cb2821bf3f8f3efb7c9191f57ce8404906f
jmargutti <jacopo.margutti@gmail.com>
2021-12-15 14:26:50 +0100

    various changes requested by SAR

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import requests
 import pandas as pd
 from collections import OrderedDict
-import io
+import smtplib, ssl, email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import os
+import pdfkit
 from dotenv import load_dotenv
-# from azure.storage.blob import BlobServiceClient
-from flask import Flask, render_template, request, send_file
+from azure.storage.blob import BlobServiceClient
+from flask import Flask, render_template, request, send_file, make_response
+from datetime import date
 app = Flask(__name__)
 load_dotenv()  # take environment variables from .env
 
 
-def process_data(df_form, rescue_number=None):
+def process_data(df_form, rescue_number=None, return_data=False, report=False):
 
     if 'rescue_number' in df_form.columns:
         rescues = df_form['rescue_number'].unique().tolist()
@@ -28,11 +34,21 @@ def process_data(df_form, rescue_number=None):
     else:
         df_form = pd.DataFrame()
 
+    columns_to_keep = ['rescue_number', 'age', 'gender', 'pregnant', 'accompanied', 'accompanied_by_who', 'country',
+                       'bracelet_number', '_submission_time']
+    for col in df_form.columns:
+        if col not in columns_to_keep:
+            df_form = df_form.drop(columns=[col])
+
     total = len(df_form)
     males, females, minors, minors_male, minors_female = 0, 0, 0, 0, 0
     pregnant, pregnant_women, pregnant_minors = 0, 0, 0
     unacc_minors, unacc_minors_male, unacc_minors_female, unacc_pregnant_minors = 0, 0, 0, 0
     unacc_women, unacc_pregnant_women = 0, 0
+    disabled, disabled_male, disabled_female = 0, 0, 0
+    age_group_counts = OrderedDict()
+    uac_age_group_counts = OrderedDict()
+    country_counts = {}
 
     if 'gender' in df_form.columns:
         males = len(df_form[df_form['gender'] == 'male'])
@@ -77,31 +93,50 @@ def process_data(df_form, rescue_number=None):
         else:
             unacc_pregnant_women = 0
 
-    # disabled
-    disabled = 0
-    if 'disabled' in df_form.columns:
-        disabled = len(df_form[df_form['disabled'] == 'yes'])
+        # disabled
+        if 'disabled' in df_form.columns:
+            df_disabled = df_form[df_form['disabled'] == 'yes']
+            disabled = len(df_disabled)
+            disabled_male = len(df_disabled[df_disabled['gender'] == 'male'])
+            disabled_female = len(df_disabled[df_disabled['gender'] == 'female'])
 
-    # age groups
-    age_group_counts = OrderedDict()
-    if 'age' in df_form.columns:
-        age_value_counts = df_form['age'].value_counts().to_dict()
-        age_label_dict = {"u1": "Less than 1 year",
-                          "1_4": "1-4 years",
-                          "5_17": "5-17 years",
-                          "18_50": "18-50 years",
-                          "50p": "More than 50 years"}
-        for age_label, age_text in age_label_dict.items():
-            if age_label in age_value_counts.keys():
-                age_group_counts[age_text] = age_value_counts[age_label]
+        # age groups
+        if 'age' in df_form.columns:
+            age_value_counts = df_form['age'].value_counts().to_dict()
+            tot = sum(age_value_counts.values())
+            age_label_dict = {"u1": "Less than 1 year",
+                              "1_4": "1-4 years",
+                              "5_17": "5-17 years",
+                              "18_50": "18-50 years",
+                              "50p": "More than 50 years"}
+            for age_label, age_text in age_label_dict.items():
+                if age_label in age_value_counts.keys():
+                    age_group_counts[age_text] = [age_value_counts[age_label], 100.*age_value_counts[age_label]/tot]
+            age_value_counts = df_unacc_minors['age'].value_counts().to_dict()
+            tot = sum(age_value_counts.values())
+            age_label_dict = {"u1": "Less than 1 year",
+                              "1_4": "1-4 years",
+                              "5_17": "5-17 years"}
+            for age_label, age_text in age_label_dict.items():
+                if age_label in age_value_counts.keys():
+                    uac_age_group_counts[age_text] = [age_value_counts[age_label], 100. * age_value_counts[age_label] / tot]
 
-    # nationalities
-    country_counts = {}
-    if 'country' in df_form.columns:
-        country_counts = df_form['country'].value_counts().to_dict()
-        country_counts = {k: v for k, v in sorted(country_counts.items(), key=lambda item: item[1], reverse=True)}
 
-    return render_template('data.html',
+
+        # nationalities
+        if 'country' in df_form.columns:
+            country_counts = df_form['country'].value_counts().to_dict()
+            tot = sum(country_counts.values())
+            country_counts = {k.replace('_', ' '): [v, 100.*v/tot] for k, v in sorted(country_counts.items(), key=lambda item: item[1], reverse=True)}
+
+    if return_data:
+        return df_form
+
+    template = 'data.html' if not report else 'report.html'
+    today = date.today()
+    dt_ = today.strftime("%d-%m-%Y")
+
+    return render_template(template,
                            total=total,
                            males=males,
                            females=females,
@@ -118,9 +153,13 @@ def process_data(df_form, rescue_number=None):
                            unacc_women=unacc_women,
                            unacc_pregnant_women=unacc_pregnant_women,
                            disabled=disabled,
+                           disabled_male=disabled_male,
+                           disabled_female=disabled_female,
                            age_value_counts=age_group_counts,
+                           uac_age_value_counts=uac_age_group_counts,
                            country_counts=country_counts,
                            rescues=rescues,
+                           date=dt_,
                            selected_rescue=str(rescue_number))
 
 
@@ -139,6 +178,14 @@ def get_data():
     return df_form
 
 
+def html_to_pdf(html, filename):
+    try:
+        pdfkit.from_string(html, filename)
+    except:
+        config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+        pdfkit.from_string(html, filename, configuration=config)
+
+
 @app.route("/data", methods=['POST'])
 def default_page():
     if request.form['password'] == os.getenv("PASSWORD"):
@@ -146,6 +193,68 @@ def default_page():
         return process_data(df_form)
     else:
         return render_template('home.html')
+
+
+@app.route("/sendreport", methods=['POST'])
+def send_report():
+    if 'rescue' in request.form.keys():
+        rescue_number = request.form['rescue']
+        email = request.form['email']
+    else:
+        rescue_number = None
+
+    today = date.today()
+    d1 = today.strftime("%d-%m-%Y")
+    filename = f"report--{d1}.html"
+
+    df_form = get_data()
+    report_html = process_data(df_form, rescue_number, report=True)
+    # html_to_pdf(report_html, filename)
+    with open(filename, "w") as file:
+        file.write(report_html)
+
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "ocean.viking.rescues@gmail.com"  # Enter your address
+    receiver_email = email  # Enter receiver address
+    password = os.getenv("PASSWORD")
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = "Ocean Viking Rescue Report"
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+    rescue_number_text = "total rescued on board"
+    if rescue_number != "total":
+        rescue_number_text = f"rescue number {rescue_number}"
+    body = f"""\
+    Dear Sir/Madam,\n
+    this is a message from the rescue vessel "Ocean Viking".\n
+    Please find attached a report of the {rescue_number_text}."""
+    message.attach(MIMEText(body, "plain"))
+
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+    # Add attachment to message and convert message to string
+    message.attach(part)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    return process_data(df_form, rescue_number)
 
 
 @app.route("/dataupdate", methods=['POST'])
@@ -158,25 +267,40 @@ def update_rescue():
     return process_data(df_form, rescue_number)
 
 
-# def get_blob_service_client(blob_path):
-#     blob_service_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION"))
-#     return blob_service_client.get_blob_client(container='ocean-viking', blob=blob_path)
+def get_blob_service_client(blob_path):
+    blob_service_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION"))
+    return blob_service_client.get_blob_client(container='ocean-viking', blob=blob_path)
 
 
-# def upload_data(data_path='rescue-data.xlsx', blob_path='website-data/rescue-data.xlsx'):
-#     blob_client = get_blob_service_client(blob_path)
-#     with open(data_path, "rb") as data:
-#         blob_client.upload_blob(data, overwrite=True)
+def upload_data(data_path, blob_path):
+    blob_client = get_blob_service_client(blob_path)
+    with open(data_path, "rb") as data:
+        blob_client.upload_blob(data, overwrite=True)
 
 
-# @app.route("/download6", methods=['GET', 'POST'])
-# def download_data(data_path='rescue-data-download.xlsx', blob_path='website-data/rescue-data.xlsx'):
-#     if os.path.exists(data_path):
-#         os.remove(data_path)
-#     blob_client = get_blob_service_client(blob_path)
-#     with open(data_path, "wb") as download_file:
-#         download_file.write(blob_client.download_blob().readall())
-#     return send_file('rescue-data-download.xlsx', as_attachment=True, attachment_filename="rescue-data.xlsx")
+@app.route("/downloaddata", methods=['POST'])
+def download_data():
+    if 'rescue' in request.form.keys():
+        rescue_number = request.form['rescue']
+    else:
+        rescue_number = None
+    df_form = get_data()
+    df_form = process_data(df_form, rescue_number, return_data=True)
+    data_path = 'rescue_data.xlsx'
+    if os.path.exists(data_path):
+        os.remove(data_path)
+    writer = pd.ExcelWriter('rescue_data.xlsx', engine='xlsxwriter')
+    df_form.to_excel(writer, sheet_name='DATA', index=False)  # send df to writer
+    worksheet = writer.sheets['DATA']  # pull worksheet object
+    for idx, col in enumerate(df_form.columns):  # loop through all columns
+        series = df_form[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+        )) + 1
+        worksheet.set_column(idx, idx, max_len)  # set column width
+    writer.save()
+    return send_file(data_path, as_attachment=True, attachment_filename="rescue-data.xlsx")
 
 
 @app.route("/")
