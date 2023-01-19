b7a264df01e5f63d7557e85164bfd0335b4bc749
jmargutti <jacopo.margutti@gmail.com>
2023-01-19 17:47:52 +0100

    bug fix: send_file argument renamed in flask 2.2.*

--- app.py
+++ app.py
@@ -519,7 +519,7 @@ def download_data():
         )) + 1
         worksheet.set_column(idx, idx, max_len)  # set column width
     writer.save()
-    return send_file(data_path, as_attachment=True, attachment_filename="rescue-data.xlsx")
+    return send_file(data_path, as_attachment=True, download_name="rescue-data.xlsx")
 
 
 @app.route("/")
